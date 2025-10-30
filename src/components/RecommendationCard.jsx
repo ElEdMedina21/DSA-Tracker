edd384cb5b7bd2e64fc40452a2b4123e5be9206a
Elias Medina <emedina21@alumnos.uaq.mx>
2025-10-29 18:56:36 -0600

    Added recommendation functionality

---
+++ src/components/RecommendationCard.jsx
@@ -0,0 +1,25 @@
+import DifficultyTag from "./DifficultyTag"
+
+export default function RecommendationCard({problem}){
+    return(
+    <div className="p-4 @container">
+        <div className="flex flex-col items-stretch justify-start rounded-xl shadow-lg bg-background-light dark:bg-[#1c2127] dark:shadow-none">
+            <div className="flex w-full min-w-72 grow flex-col items-stretch justify-center gap-4 p-6 sm:p-8">
+                <div className="flex justify-between items-start gap-4">
+                    <h2 className="text-[#333333] dark:text-white text-2xl font-bold leading-tight tracking-[-0.015em]">
+                    {problem.problemName}
+                    </h2>
+                    <DifficultyTag difficulty={problem.difficulty}/>
+                </div>
+                {/* <p className="text-[#333333] dark:text-[#9dabb9] text-base font-normal leading-relaxed">
+                    Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+                </p> */}
+                <div className="flex items-center justify-end gap-3 pt-4">
+                    <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-12 px-6 bg-primary text-white text-base font-bold leading-normal tracking-[0.015em] hover:bg-primary/90 transition-colors">
+                    <span className="truncate">Solve Problem</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>)
+}
--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -1,6 +1,22 @@
 import { ArrowPathIcon, FunnelIcon } from "@heroicons/react/16/solid"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { backendURL } from "../utils/backendUrl"
+import RecommendationCard from "./RecommendationCard"
 
 export default function Recommendations(){
+
+    const [recommendations, setRecommendations] = useState()
+
+    const getRecommendations = async()=>{
+        const {data} = await axios.get(`${backendURL}/recommendations`)
+        setRecommendations(data)
+    }
+
+    useEffect(()=>{
+        getRecommendations()
+    }, [])
+
     return(
         <>
             {/* Header */}
@@ -11,29 +27,8 @@ export default function Recommendations(){
             </div>
 
             {/* Recommendation Card */}
-            <div className="p-4 @container">
-                <div className="flex flex-col items-stretch justify-start rounded-xl shadow-lg bg-background-light dark:bg-[#1c2127] dark:shadow-none">
-                    <div className="flex w-full min-w-72 grow flex-col items-stretch justify-center gap-4 p-6 sm:p-8">
-                        <div className="flex justify-between items-start gap-4">
-                            <h2 className="text-[#333333] dark:text-white text-2xl font-bold leading-tight tracking-[-0.015em]">
-                            Find the Missing Number
-                            </h2>
-                            <span className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-[#F5A623]/20 text-[#F5A623]">
-                            Medium
-                            </span>
-                        </div>
-                        <p className="text-[#333333] dark:text-[#9dabb9] text-base font-normal leading-relaxed">
-                            Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
-                        </p>
-                        <div className="flex items-center justify-end gap-3 pt-4">
-                            <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-12 px-6 bg-primary text-white text-base font-bold leading-normal tracking-[0.015em] hover:bg-primary/90 transition-colors">
-                            <span className="truncate">Solve Problem</span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            {recommendations && 
+            <RecommendationCard problem={recommendations[0]}/>}
             {/* Footer Buttons */}
             <div className="flex justify-stretch">
                 <div className="flex flex-1 gap-3 flex-wrap px-4 py-3 justify-start">
